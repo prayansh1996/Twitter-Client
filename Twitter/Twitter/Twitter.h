8bd2ad25ee627cc60e684ea9f3ed75108395b898
Prayansh Srivastava <prayansh1996@gmail.com>
2016-11-23 18:18:16 +0530

    Add files via upload

---
+++ Twitter/Twitter/Twitter.h
@@ -0,0 +1,19 @@
+//
+//  Twitter.h
+//  Twitter
+//
+//  Created by Krishna on 26/10/16.
+//  Copyright © 2016 Mehuls. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Twitter.
+FOUNDATION_EXPORT double TwitterVersionNumber;
+
+//! Project version string for Twitter.
+FOUNDATION_EXPORT const unsigned char TwitterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Twitter/PublicHeader.h>
+
+
